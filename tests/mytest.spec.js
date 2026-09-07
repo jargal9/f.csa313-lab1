e1bfdde9f6c40127b16448577587ead644c666d1
Oyunjargal <jargal130613@gmail.com>
2026-09-08 03:14:26 +0800

    Эхний login тест

---
+++ tests/mytest.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('амжилттай нэвтрэх', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(
+    page.getByText('Products', { exact: true })
+  ).toBeVisible();
+
+  await expect(page).toHaveURL(/inventory.html/);
+});
